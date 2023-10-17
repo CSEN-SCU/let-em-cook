7dbe6c5827a5a879057bc8e6ec8cc4efad9dbdad
Arnav Choudhury <66573183+8coolguy@users.noreply.github.com>
2023-10-17 14:48:30 -0700

    swift-env

---
+++ Let'em Cook/Let_em_CookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Let_em_CookApp.swift
+//  Let'em Cook
+//
+//  Created by Arnav Choudhury on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Let_em_CookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
